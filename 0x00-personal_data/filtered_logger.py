ad8a5d0d5565baf97864e921dd65bcdf98a53359
Queendarlin <queendarlinnnamani@gmail.com>
2024-07-31 23:53:55 +0000

    Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -56,3 +56,31 @@ class RedactingFormatter(logging.Formatter):
         message = super().format(record)
         return filter_datum(self.fields, self.REDACTION, message,
                             self.SEPARATOR)
+
+
+# Define PII_FIELDS with a tuple of fields considered as PII
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+def get_logger() -> logging.Logger:
+    """
+    Create a logger with the name 'user_data' and
+    configure it with a RedactingFormatter.
+
+    Returns:
+        logging.Logger: Configured logger instance.
+    """
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False  # Disable propagation
+
+    # Create a StreamHandler
+    handler = logging.StreamHandler()
+    # Set the formatter to RedactingFormatter with PII_FIELDS
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    handler.setFormatter(formatter)
+
+    # Add the handler to the logger
+    logger.addHandler(handler)
+
+    return logger
