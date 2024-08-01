0f50bb54e20005445218579a8bed6338f25b9906
Queendarlin <queendarlinnnamani@gmail.com>
2024-08-01 08:44:30 +0000

    Encrypting passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,20 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Module for encrypting password
+"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+        Hashes a password with a randomly-generated salt and
+        returns the hashed password.
+
+        Args:
+            password (str): The password to hash.
+
+        Returns:
+            bytes: The salted and hashed password.
+        """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
