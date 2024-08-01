0273660dfc9a3c5c18b7ab75503a9397c909d279
Queendarlin <queendarlinnnamani@gmail.com>
2024-08-01 11:50:13 +0000

    environment variable

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,9 +7,6 @@ import os
 from typing import List
 import logging
 import mysql.connector
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
