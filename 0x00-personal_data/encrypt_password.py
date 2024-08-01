e29a4892e66a2970f3b9022220ba9aed6ee9e12d
Queendarlin <queendarlinnnamani@gmail.com>
2024-08-01 08:56:56 +0000

    Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -18,3 +18,18 @@ def hash_password(password: str) -> bytes:
             bytes: The salted and hashed password.
         """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+        Validates that the provided password matches the hashed password.
+
+        Args:
+            hashed_password (bytes): The hashed password to check against.
+            password (str): The plain text password to validate.
+
+        Returns:
+            bool: True if the password matches the hashed password,
+            False otherwise.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
