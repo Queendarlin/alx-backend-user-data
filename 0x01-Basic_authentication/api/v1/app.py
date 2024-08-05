76bcb08b48c39d9e8a27ec4425c0e5ca664b9a8b
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-08-05 22:02:31 +0100

    Before Request

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,11 +15,10 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
-auth_type = getenv('AUTH_TYPE', 'auth')
-if auth_type == 'auth':
-    auth = Auth()
-if auth_type == 'basic_auth':
-    auth = BasicAuth()
+AUTH_TYPE = getenv("AUTH_TYPE")
+if AUTH_TYPE:
+    if AUTH_TYPE == 'auth':
+        auth = Auth()
 
 
 @app.errorhandler(404)
