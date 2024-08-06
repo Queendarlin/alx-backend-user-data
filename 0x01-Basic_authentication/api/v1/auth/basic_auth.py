52b59f19d242ae573142675df852708c3111c819
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-08-06 07:47:51 +0100

    Retrieves the User instance for a request

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -66,3 +66,20 @@ class BasicAuth(Auth):
             if users[0].is_valid_password(user_pwd):
                 return users[0]
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieves the User instance for a request"""
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+        base64_header = self.extract_base64_authorization_header(auth_header)
+        if base64_header is None:
+            return None
+        decoded_header =\
+            self.decode_base64_authorization_header(base64_header)
+        if decoded_header is None:
+            return None
+        email, password = self.extract_user_credentials(decoded_header)
+        if email is None or password is None:
+            return None
+        return self.user_object_from_credentials(email, password)
