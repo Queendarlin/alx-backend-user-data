4b6ca890c728bcc04183d718ead4fcf95148dd59
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-08-07 22:37:09 +0100

    Session auth module

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Module for Session authentication class"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Class for session authentication"""
+    pass
