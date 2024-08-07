90fe9d5d7d37bb65e53da7c5676dd4a2522ff7bf
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-08-07 22:43:23 +0100

    Session auth module

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -17,7 +17,7 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
-auth_type = getenv("AUTH_TYPE")
+auth_type = getenv('AUTH_TYPE', 'auth')
 if auth_type == 'auth':
     auth = Auth()
 if auth_type == 'basic_auth':
