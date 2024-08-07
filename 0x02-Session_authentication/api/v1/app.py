ac26019c8f7a594985a07565c58aec0066fd0699
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-08-07 22:42:29 +0100

    Session auth module

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -18,11 +18,16 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
 auth_type = getenv("AUTH_TYPE")
-if auth_type:
-    if auth_type == 'basic_auth':
-        auth = BasicAuth()
-    elif auth_type == "session_auth":
-        auth = SessionAuth()
+if auth_type == 'auth':
+    auth = Auth()
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
+if auth_type == 'session_auth':
+    auth = SessionAuth()
+if auth_type == 'session_exp_auth':
+    auth = SessionExpAuth()
+if auth_type == 'session_db_auth':
+    auth = SessionDBAuth()
 
 
 @app.errorhandler(404)
