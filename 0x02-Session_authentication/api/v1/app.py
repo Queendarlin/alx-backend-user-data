732d8bb9b15a35e730804cfd20cc239dae6c5705
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-08-08 11:26:39 +0100

    Sessions in database

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -19,14 +19,16 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
-auth_type = getenv("AUTH_TYPE")
+auth_type = getenv("AUTH_TYPE", 'auth')
+if auth_type == 'auth':
+    auth = Auth()
 if auth_type == 'basic_auth':
     auth = BasicAuth()
-elif auth_type == 'session_auth':
+if auth_type == 'session_auth':
     auth = SessionAuth()
-elif auth_type == 'session_exp_auth':
+if auth_type == 'session_exp_auth':
     auth = SessionExpAuth()
-elif auth_type == 'session_db_auth':
+if auth_type == 'session_db_auth':
     auth = SessionDBAuth()
 
 
