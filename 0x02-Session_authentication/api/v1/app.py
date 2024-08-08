cb634c5dfaf6a54c3a7a248706c012c028139a18
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-08-08 18:02:50 +0100

    resolving issues

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -61,10 +61,10 @@ def before_request_func():
             "/api/v1/auth_session/login/",
         ]
         if auth.require_auth(request.path, excluded_paths):
-            user = auth.current_user(request)
             if auth.authorization_header(request) is None and \
                     auth.session_cookie(request) is None:
                 abort(401)
+            user = auth.current_user(request)
             if user is None:
                 abort(403)
             request.current_user = user
