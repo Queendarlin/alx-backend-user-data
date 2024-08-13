4340b75a3817f612308a55a9ae742f36b963bb00
Queendarlin <queendarlinnnamani@gmail.com>
2024-08-13 14:59:17 +0000

    Update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -122,6 +122,7 @@ def get_reset_password_token():
 
 @app.route('/reset_password', methods=['PUT'])
 def update_password():
+    """ Update user password endpoint """
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
     new_password = request.form.get('new_password')
