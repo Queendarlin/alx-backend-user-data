ca6c0ce5cd45e56340c0c466461091c787a89551
Queendarlin <queendarlinnnamani@gmail.com>
2024-08-13 09:40:44 +0000

    Method to find a particular user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
 
 from user import Base, User
 
@@ -34,11 +37,29 @@ class DB:
         """
         Method to add a user to the database and return the User object
         """
+        user = User(email=email, hashed_password=hashed_password)
+        session = self._session
+        session.add(user)
+        session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Finds a user by arbitrary keyword arguments.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments to filter users.
+
+        Returns:
+            The first User object found based on the filter criteria.
+
+        Raises:
+            NoResultFound: If no user is found with the given filter criteria.
+            InvalidRequestError: If the filter criteria are invalid.
+        """
         try:
-            new_user = User(email=email, hashed_password=hashed_password)
-            self._session.add(new_user)
-            self._session.commit()
-        except Exception:
-            self._session.rollback()
-            new_user = None
-        return new_user
+            user = self._session.query(User).filter_by(**kwargs).one()
+            return user
+        except NoResultFound:
+            raise NoResultFound("No user found with the attributes.")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid filter criteria provided.")
