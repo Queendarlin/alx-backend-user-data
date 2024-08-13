76b750e75d22763116b90fa6ef71aff217de796a
Queendarlin <queendarlinnnamani@gmail.com>
2024-08-13 14:07:37 +0000

    Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -42,9 +42,14 @@ class Auth:
 
     def valid_login(self, email: str, password: str) -> bool:
         """Validates login credentials."""
+        user = None
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'),
-                                  user.hashed_password)
+            if user is not None:
+                return bcrypt.checkpw(
+                    password.encode("utf-8"),
+                    user.hashed_password,
+                )
         except NoResultFound:
             return False
+        return False
