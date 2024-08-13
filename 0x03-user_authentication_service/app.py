5f5b6ab6019fe0c63cf9c537014207f1fa171c0a
Queendarlin <queendarlinnnamani@gmail.com>
2024-08-13 10:46:17 +0000

    Flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,20 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Flask app for user authentication.
+"""
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def index() -> jsonify:
+    """Handles the GET request for the root route.
+
+    Returns:
+        jsonify: JSON response with a welcome message.
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
